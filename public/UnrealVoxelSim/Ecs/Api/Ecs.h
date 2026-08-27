cecb1d09d5c1ed31cb27db0d551d05b294512ea5
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-08-27 18:41:29 +0200

    Remove proxy header

--- public/UnrealVoxelSim/Ecs/Api/Ecs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "UnrealVoxelSim/Ecs/Api/ComponentOperationError.h"
-#include "UnrealVoxelSim/Ecs/Api/EntityId.h"
-#include "UnrealVoxelSim/Ecs/Api/EntityOperationError.h"
-#include "UnrealVoxelSim/Ecs/Api/Query.h"
-#include "UnrealVoxelSim/Ecs/Api/Registry.h"
-#include "UnrealVoxelSim/Ecs/Api/RegistryScopeId.h"
